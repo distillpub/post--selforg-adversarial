29b84355076eea2861d21b1b9eeba2fd03719a45
Alexander Mordvintsev <zzznah@gmail.com>
2020-11-25 09:55:16 +0100

    fix standalone demos

--- public/mutating_gca_demo.js
+++ public/mutating_gca_demo.js
@@ -1,14 +1,15 @@
 'use strict';
 
 function isInViewport(element) {
-  const frameRect = window.frameElement.getBoundingClientRect();
   const rect = element.getBoundingClientRect();
-  const px = frameRect.left;
-  const py = frameRect.top;
+  const frame = window.frameElement;
+  const frameRect = frame ? frame.getBoundingClientRect() : null;
+  const [px, py] = frame ? [frameRect.left, frameRect.top] : [0, 0];
   const w = window.top.innerWidth;
   const h = window.top.innerHeight;
   return rect.top+py < h && rect.left+px < w && rect.bottom+py > 0 && rect.right+px > 0;
 }
+
 function mutatingGCADemo() {
   const $ = q=>document.querySelector(q);
 
